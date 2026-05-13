45f79c504d075eeca9bd9d1ec911ca6e2e7550c5
Alejo <alejo.lcc@gmail.com>
2026-05-13 12:08:54 -0300

    bump version

--- mix.exs
+++ mix.exs
@@ -1,13 +1,19 @@
 defmodule ObanChore.MixProject do
   use Mix.Project
 
+  @version "0.1.0-beta"
+
   def project do
     [
       app: :oban_chore,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: description(),
+      package: package(),
+      deps: deps(),
+      name: "ObanChore",
+      source_url: "https://github.com/alejolcc/oban_chore"
     ]
   end
 
@@ -19,13 +25,26 @@ defmodule ObanChore.MixProject do
     ]
   end
 
+  defp description do
+    "UI-driven operational tools for Oban workers."
+  end
+
+  defp package do
+    [
+      maintainers: ["Alejo"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/alejolcc/oban_chore"}
+    ]
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:oban, "~> 2.18"},
       {:phoenix_live_view, "~> 1.0"},
       {:phoenix_html, "~> 4.3"},
-      {:ecto, "~> 3.10"}
+      {:ecto, "~> 3.10"},
+      {:ex_doc, "~> 0.31", runtime: false, only: :dev}
     ]
   end
 end
